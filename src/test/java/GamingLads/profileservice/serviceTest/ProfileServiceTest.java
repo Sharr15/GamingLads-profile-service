3fb29ee4000ad2b7847c885dd822d18ab4734930
Sharr15 <429562@student.fontys.nl>
2020-11-12 16:19:57 +0100

    Removed unneccesary dependencies

--- src/test/java/GamingLads/profileservice/serviceTest/ProfileServiceTest.java
+++ src/test/java/GamingLads/profileservice/serviceTest/ProfileServiceTest.java
@@ -7,17 +7,14 @@ import GamingLads.profileservice.service.ProfileService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 class ProfileServiceTest {
 
